3e502ca5dd067821923695e0641ae869ea6ac8af
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2020-01-02 15:29:31 +0200

    configuration changes on mainflux config files to fit our case.

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -6,4 +6,10 @@ else
     sed  -e "s/UI_PORT/3000/" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
+if [ -n "$UI_INSIGHIO_PORT" ]; then
+    sed -i -e "s/UI_INSIGHIO_PORT/$UI_INSIGHIO_PORT/" /etc/nginx/nginx.conf
+else
+    sed -i -e "s/UI_INSIGHIO_PORT/3004/" /etc/nginx/nginx.conf
+fi
+
 exec nginx -g "daemon off;"
