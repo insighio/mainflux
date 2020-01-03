5b3769ebf4349081af4982da87e62dc71fc9fab7
ci <ci@insigh.io>
2020-01-03 12:09:08 +0100

    setup authentication on influxdb access.  - added read/write user and read-only user.  - for now, the read/write user is used by influxdb-writer.

--- composeUp.sh
+++ composeUp.sh
@@ -7,4 +7,6 @@ UI_PORT=3000 \
 MF_MQTT_ADAPTER_PORT=1884 \
 RPROXY_PORT=3003 \
 UI_INSIGHIO_PORT=3004 \
+MF_INFLUX_WRITER_DB_USER="insighio" \
+MF_INFLUX_WRITER_DB_PASS="insighio" \
 docker-compose -f docker/docker-compose.yml -f docker/addons/influxdb-writer/docker-compose.yml -f docker/addons/bootstrap/docker-compose.yml -f docker/addons/lora-adapter/docker-compose.yml up -d
