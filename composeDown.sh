3d055b88baf1932c3a954f8e0639ac74dd84def3
ci <ci@insigh.io>
2020-01-03 17:23:45 +0100

    Move all localy configured environment variables to .env file.

--- composeDown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MF_UI_PORT=3000 \
-UI_PORT=3000 \
-MF_MQTT_ADAPTER_PORT=1884 \
-RPROXY_PORT=3003 \
-UI_INSIGHIO_PORT=3004 \
-docker-compose -f docker/docker-compose.yml -f docker/addons/influxdb-writer/docker-compose.yml -f docker/addons/bootstrap/docker-compose.yml -f docker/addons/lora-adapter/docker-compose.yml down
