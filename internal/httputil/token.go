183824d427ae178408b19a0aacff33ed54b0dcee
Kostas Balampekos <kostas.balampekos@insigh.io>
2023-12-14 15:00:34 +0200

    CNSL-12 Updated token extraction to not expect the "Bearer " prefix to align with the other requests in this version

--- internal/httputil/token.go
+++ internal/httputil/token.go
@@ -5,22 +5,11 @@ package httputil
 
 import (
 	"net/http"
-	"strings"
 )
 
-// BearerPrefix represents the token prefix for Bearer authentication scheme.
-const BearerPrefix = "Bearer "
-
-// ThingPrefix represents the key prefix for Thing authentication scheme.
-const ThingPrefix = "Thing "
-
-// ExtractBearerToken returns value of the bearer token. If there is no bearer token - an empty value is returned.
+// ExtractBearerToken returns value of the token.
 func ExtractBearerToken(r *http.Request) string {
 	token := r.Header.Get("Authorization")
 
-	if !strings.HasPrefix(token, BearerPrefix) {
-		return ""
-	}
-
-	return strings.TrimPrefix(token, BearerPrefix)
+	return token
 }
